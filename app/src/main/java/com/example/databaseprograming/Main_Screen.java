bc205c7fcb2bb83c1329df61059b972c0b29cba8
FCha <ghksdlqhdl@naver.com>
2023-11-13 20:14:54 +0900

    중간 푸시

--- app/src/main/java/com/example/databaseprograming/Main_Screen.java
+++ app/src/main/java/com/example/databaseprograming/Main_Screen.java
@@ -1,32 +1,30 @@
 package com.example.databaseprograming;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main_Screen extends AppCompatActivity {
 
     RecyclerView hospital_list;
-    hospital_recyle_adapter hadapter;
-    ArrayList<String> hospital_text;
+    Main_hospital_type_recycleradapter hType_recycler;
     ArrayList<Integer> hospital_icon;
+    ArrayList<String> hospital_text;
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
         //과목별 recyclerview 리스트 추가
         hospital_text = new ArrayList<>(
-                Arrays.asList("이비인후과", "내과", "정형외과", "안과", "소아과")
+                Arrays.asList("이비인후과", "내과", "정형외과", "안과", "소아청소년과", "산부인과", "신경외과", "피부과", "정신건강의학과")
         );
         hospital_icon = new ArrayList<>();
         //integerArrayList.add(R.drawable.imageName);
@@ -35,14 +33,9 @@ public class Main_Screen extends AppCompatActivity {
         hospital_list = findViewById(R.id.hospital_type_recycle);
 
         //리사이클러뷰 어댑터 초기화
-        hadapter = new hospital_recyle_adapter(hospital_text, hospital_icon);
-
-        //리사이클러뷰 세팅
-        hospital_list.setAdapter(hadapter);
         hospital_list.setLayoutManager(new LinearLayoutManager(this));
-
-
-
+        hType_recycler = new Main_hospital_type_recycleradapter(hospital_icon, hospital_text);
+        hospital_list.setAdapter(hType_recycler);
 
 
     }
---
+++ app/src/main/java/com/example/databaseprograming/Main_hospital_type_recycleradapter.java
@@ -0,0 +1,64 @@
+package com.example.databaseprograming;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Main_hospital_type_recycleradapter extends RecyclerView.Adapter<Main_hospital_type_recycleradapter.ViewHolder>{
+
+    private ArrayList<Integer> typeIcon = null;
+    private ArrayList<String> typeText = null;
+
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageButton imageButton1;
+        TextView textView1 ;
+
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            imageButton1 = itemView.findViewById(R.id.main_recycler_hospital_icon);
+            textView1 = itemView.findViewById(R.id.main_recycler_hospital_text) ;
+        }
+    }
+
+    Main_hospital_type_recycleradapter(ArrayList<Integer> tIcon, ArrayList<String> tText) {
+         typeIcon = tIcon;
+         typeText = tText;
+    }
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
+    @Override
+    public Main_hospital_type_recycleradapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.hospital_type, parent, false) ;
+        Main_hospital_type_recycleradapter.ViewHolder vh = new Main_hospital_type_recycleradapter.ViewHolder(view) ;
+
+        return vh ;
+    }
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @Override
+    public void onBindViewHolder(Main_hospital_type_recycleradapter.ViewHolder holder, int position) {
+        Integer image = typeIcon.get(position);
+        String text = typeText.get(position) ;
+        holder.imageButton1.setImageResource(image);
+        holder.textView1.setText(text) ;
+    }
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    @Override
+    public int getItemCount() {
+        return typeIcon.size() ;
+    }
+}
